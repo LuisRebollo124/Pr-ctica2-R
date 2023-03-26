7b043eb242c9317ebf95a4deb3aed5fdd500f828
Luis Rebollo <luisrebollo30@gmail.com>
2023-03-26 16:28:46 +0200

    Inicio

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
+</head>
+<body>
+    <main>
+        <header><h1>Votación de la mejor película</h1></header>
+        <article></article>
+        <article></article>
+    </main>
+</body>
+</html>
